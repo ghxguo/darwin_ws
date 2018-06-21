fb651edf7aaaa0fbffd1c8e796b1c41151b65f87
Howard <hongxug5@gmail.com>
2018-06-21 05:39:07 +0000

    exclude unwated files

--- build/camera_stream/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/darwin_ws/src/camera_stream
-# Build directory: /home/pi/darwin_ws/build/camera_stream
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
